14c6969198e8b8ab3e17ac3b5fefa779f25f1ed6
Maurice Schippers <476719@student.fontys.nl>
2023-05-31 23:12:07 +0200

    Accidentally imported the wrong dependency.

--- user-matching-service/src/test/java/com/cinematch/usermatchingservice/UserMatchingServiceApplicationTests.java
+++ user-matching-service/src/test/java/com/cinematch/usermatchingservice/UserMatchingServiceApplicationTests.java
@@ -1,13 +1,21 @@
 package com.cinematch.usermatchingservice;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class UserMatchingServiceApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
--- user-preferences-service/src/test/java/com/cinematch/userpreferencesservice/UserPreferencesServiceApplicationTests.java
+++ user-preferences-service/src/test/java/com/cinematch/userpreferencesservice/UserPreferencesServiceApplicationTests.java
@@ -2,7 +2,7 @@ package com.cinematch.userpreferencesservice;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.apache.catalina.core.ApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
