d35c0533ccda03b8923d0da338014c5764a317bd
Maurice Schippers <476719@student.fontys.nl>
2023-06-05 23:51:31 +0200

    Tests should pass now!

--- user-data-service/src/test/java/com/cinematch/userdataservice/UserDataServiceApplicationTests.java
+++ user-data-service/src/test/java/com/cinematch/userdataservice/UserDataServiceApplicationTests.java
@@ -3,19 +3,23 @@ package com.cinematch.userdataservice;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import com.cinematch.userdataservice.config.H2JpaConfig;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = { UserDataServiceApplication.class, H2JpaConfig.class })
+@ActiveProfiles("test")
 class UserDataServiceApplicationTests {
 
-	// @Autowired
-	// ApplicationContext context;
+	@Autowired
+	ApplicationContext context;
 
-	// @Test
-	// void contextLoads() {
-	// 	assertNotNull(context);
-	// }
+	@Test
+	void contextLoads() {
+		assertNotNull(context);
+	}
 
 }
