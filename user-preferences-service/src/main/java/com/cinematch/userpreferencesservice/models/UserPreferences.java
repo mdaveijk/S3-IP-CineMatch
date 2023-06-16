0ce9d38e50e801a9ca2082a4865cfed42d594815
Maurice Schippers <476719@student.fontys.nl>
2023-06-16 18:42:27 +0200

    First working version of using Polymorphism.

--- user-preferences-service/src/main/java/com/cinematch/userpreferencesservice/models/UserPreferences.java
+++ user-preferences-service/src/main/java/com/cinematch/userpreferencesservice/models/UserPreferences.java
@@ -10,41 +10,56 @@ public class UserPreferences {
     @Id
     private Long userId;
 
-    private List<String> preferences;
+    //This would have been done through polymorphism in a normal scenario.
+    private List<MoviePreference> moviePreferences;
+    
+    private List<GenrePreference> genrePreferences;
 
+    
     private String location;
 
     public Long getUserId() {
         return userId;
     }
-
+    
     public void setUserId(Long userId) {
         this.userId = userId;
     }
-
-    public List<String> getPreferences() {
-        return preferences;
+    
+    public List<MoviePreference> getMoviePreferences() {
+        return moviePreferences;
     }
 
-    public void setPreferences(List<String> preferences) {
-        this.preferences = preferences;
+    public void setMoviePreferences(List<MoviePreference> moviePreferences) {
+        this.moviePreferences = moviePreferences;
+    }
+
+    public List<GenrePreference> getGenrePreferences() {
+        return genrePreferences;
+    }
+
+    public void setGenrePreferences(List<GenrePreference> genrePreferences) {
+        this.genrePreferences = genrePreferences;
     }
 
     public String getLocation() {
         return location;
     }
-
+    
     public void setLocation(String location) {
         this.location = location;
     }
-
+    
     public UserPreferences() {
         
     }
 
-    public UserPreferences(Long id, List<String> preferences, String location) {
-        this.userId = id;
-        this.preferences = preferences;
-        this.location = location;
+    public UserPreferences(Long userId, List<MoviePreference> moviePreferences, List<GenrePreference> genrePreferences,
+        String location) {
+    this.userId = userId;
+    this.moviePreferences = moviePreferences;
+    this.genrePreferences = genrePreferences;
+    this.location = location;
     }
+
 }
