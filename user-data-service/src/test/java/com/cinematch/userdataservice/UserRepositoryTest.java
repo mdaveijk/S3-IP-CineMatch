b31282fcd263b262b866461ac264b5ccffbf39f9
Maurice Schippers <476719@student.fontys.nl>
2023-06-17 23:13:37 +0200

    First successfull tests are written.

--- user-data-service/src/test/java/com/cinematch/userdataservice/UserRepositoryTest.java
+++ user-data-service/src/test/java/com/cinematch/userdataservice/UserRepositoryTest.java
@@ -10,7 +10,7 @@ import org.mockito.Mockito;
 import com.cinematch.userdataservice.models.User;
 import com.cinematch.userdataservice.repositories.UserRepository;
 
-public class UserDataRepositoryTest {
+public class UserRepositoryTest {
     
     private UserRepository repository;
 
